86d2beb4f9569bce3a0053b4a09707b8078a2c8b
maydek1 <maydekcraft@gmail.com>
2024-10-13 14:03:10 +0300

    init project

---
+++ src/test/kotlin/com/software/modsen/carservice/CarServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.software.modsen.carservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
