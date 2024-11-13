2c58a93fa0a59c7e7326f327b7c46281092635cc
maydek1 <maydekcraft@gmail.com>
2024-11-13 10:29:02 +0300

    feat(kotlin): integrate coroutines for asynchronous operations

--- src/main/kotlin/com/software/modsen/carservice/service/CarService.kt
+++ src/main/kotlin/com/software/modsen/carservice/service/CarService.kt
@@ -7,28 +7,34 @@ import com.software.modsen.carservice.model.Car
 import com.software.modsen.carservice.repository.CarRepository
 import com.software.modsen.carservice.util.carNumberAlreadyExists
 import com.software.modsen.carservice.util.carNotFound
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 
 @Service
 class CarService(
-        private val carRepository: CarRepository,
-        private val modelMapper: ModelMapper
+    private val carRepository: CarRepository,
+    private val modelMapper: ModelMapper
 ) {
 
-    fun getCarById(id: Long): Car {
+    suspend fun getCarById(id: Long): Car {
         return getOrElseThrow(id)
     }
 
-    fun deleteCarById(id: Long): Car {
+    suspend fun deleteCarById(id: Long): Car {
         val car = getOrElseThrow(id)
-        carRepository.deleteById(id)
+        withContext(Dispatchers.IO) {
+            carRepository.deleteById(id)
+        }
         return car
     }
 
-    fun updateCar(id: Long, carRequest: CarRequest): Car {
-        val car = carRepository.findById(id).orElseThrow {
-            CarNotFoundException(carNotFound(id))
+    suspend fun updateCar(id: Long, carRequest: CarRequest): Car {
+        val car = withContext(Dispatchers.IO) {
+            carRepository.findById(id).orElseThrow {
+                CarNotFoundException(carNotFound(id))
+            }
         }
 
         checkNumberToExist(car.number, carRequest.number)
@@ -36,30 +42,38 @@ class CarService(
         val updatedCar = modelMapper.map(carRequest, Car::class.java).apply {
             this.id = car.id
         }
-        return carRepository.save(updatedCar)
+        return withContext(Dispatchers.IO) {
+            carRepository.save(updatedCar)
+        }
     }
 
-    fun createCar(carRequest: CarRequest): Car {
-        if (carRepository.existsByNumber(carRequest.number)) {
+    suspend fun createCar(carRequest: CarRequest): Car {
+        if (withContext(Dispatchers.IO) { carRepository.existsByNumber(carRequest.number) }) {
             throw CarNumberAlreadyExistException(carNumberAlreadyExists(carRequest.number))
         }
 
         val car = modelMapper.map(carRequest, Car::class.java)
-        return carRepository.save(car)
-    }
-
-    private fun getOrElseThrow(id: Long): Car {
-        return carRepository.findById(id).orElseThrow {
-            CarNotFoundException(carNotFound(id))
+        return withContext(Dispatchers.IO) {
+            carRepository.save(car)
         }
     }
 
-    fun getAllCars(): List<Car> {
-        return carRepository.findAll()
+    private suspend fun getOrElseThrow(id: Long): Car {
+        return withContext(Dispatchers.IO) {
+            carRepository.findById(id).orElseThrow {
+                CarNotFoundException(carNotFound(id))
+            }
+        }
     }
 
-    private fun checkNumberToExist(currentNumber: String, newNumber: String) {
-        if (currentNumber != newNumber && carRepository.existsByNumber(newNumber)) {
+    suspend fun getAllCars(): List<Car> {
+        return withContext(Dispatchers.IO) {
+            carRepository.findAll()
+        }
+    }
+
+    private suspend fun checkNumberToExist(currentNumber: String, newNumber: String) {
+        if (currentNumber != newNumber && withContext(Dispatchers.IO) { carRepository.existsByNumber(newNumber) }) {
             throw CarNumberAlreadyExistException(carNumberAlreadyExists(newNumber))
         }
     }
